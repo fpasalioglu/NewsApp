29253455b79efd479f2de5e7c961678987ea30a7
fpasalioglu <furkanpasalioglu61@gmail.com>
2021-10-03 23:23:36 +0300

    Model tanımlama

---
+++ app/src/main/java/com/furkanpasalioglu/newsapp/models/Source.kt
@@ -0,0 +1,11 @@
+package com.furkanpasalioglu.newsapp.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Source(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("name")
+    val name: String
+)
